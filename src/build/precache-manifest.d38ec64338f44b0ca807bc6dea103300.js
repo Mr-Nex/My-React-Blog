81c90280411fc35c3a54a7ab98fc26dd9a2fa5f4
Mr-Nex <nitink386@gmail.com>
2020-08-01 13:41:46 +0530

    Blog Commit

---
+++ src/build/precache-manifest.d38ec64338f44b0ca807bc6dea103300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3965508c04a587d11768db720f9539b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a829f82b38ceb2a9973",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e1d8184cbf649012162f",
+    "url": "/static/js/2.cd67acc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd67acc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a829f82b38ceb2a9973",
+    "url": "/static/js/main.40325bd5.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
